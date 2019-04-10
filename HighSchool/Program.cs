65ca6f5ca30d2b9fddd4a27247416c59a5cc4dc5
Inna Davis <idavis@cordelllaw.com>
2019-04-09 19:59:27 -0500

    lasest code

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
@@ -6,6 +7,29 @@ namespace HighSchool
     {
         public static void Main()
         {
+
+            var students = new List<Student>
+            {
+                new Student { FirstName = "FirstOne", LastName = "LastOne", GPA = 2.0, Grade = 6 },
+                new Student { FirstName = "FirstTwo", LastName = "LastTwo", GPA = 3.1, Grade = 9 },
+                new Student { FirstName = "FirstThree", LastName = "LastThree", GPA = 3.2, Grade = 5 },
+                new Student { FirstName = "FirstFourB", LastName = "LastFourB", GPA = 3.0, Grade = 9 },
+                new Student { FirstName = "FirstFourC", LastName = "LastFourA", GPA = 4.3, Grade = 6 },
+                new Student { FirstName = "FirstFourA", LastName = "LastFourA", GPA = 1.8, Grade = 9 },
+                new Student { FirstName = "FirstFive", LastName = "LastFive", GPA = 2.0, Grade = 8 },
+                new Student { FirstName = "FirstSix", LastName = "LastSix", GPA = 3.1, Grade = 12 },
+                new Student { FirstName = "FirstSeven", LastName = "LastSeven", GPA = 1.2, Grade = 5 },
+                new Student { FirstName = "FirstEight", LastName = "LastEight", GPA = 2.3, Grade = 12 }
+            };
+
+            var result = Student.StarStudents(students);
+
+            foreach (var student in result)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} {student.Grade} {student.GPA}");
+            }
+  
+
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -1,12 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace HighSchool
 {
+
     public class Student
     {
+        //private int _grade;
+        //private double _gpa;
+        public static double qualifyingGrade = 3;
+        public string FirstName { set; get; }
+        public string LastName { get; set; }
+     
+        public double GPA { set; get; }
+      
+        public int Grade { set; get; }
+
+        public Student (double GPA, int Grade)
+        {
+            GPA = Math.Max(0, 4);
+            GPA = Math.Min(0, 4);
+
+            Grade = Math.Max(1, 12);
+            Grade = Math.Min(1, 12);
+        }
+
+      
+
+
         public static List<Student> StarStudents(List<Student> students)
         {
+            var starStudents = students.Where(student => student.GPA >= qualifyingGrade && (student.Grade >= 9 && student.Grade <= 12))
+                                        .OrderByDescending(x => x.Grade)
+                                        .ThenByDescending(x => x.GPA)
+                                        .ThenBy(x => x.LastName)
+                                        .ThenBy(x => x.FirstName)
+                                        .ToList();
+         
+            return starStudents;
         }
+
+        public int GetGradeMax(List<Student> students)
+        {
+            return students.Select(student => student.Grade).Max();
+        }
+
+        public double GetGPAMax(List<Student> students)
+        {
+            return students.Select(student => student.GPA).Max();
+        }
+
     }
 }
